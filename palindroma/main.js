b7a4d961a436e7a449eeae125c74a5dd58a750fb
federicoalagna <federicoalagna01@gmail.com>
2024-05-07 14:22:57 +0200

    funzione

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const parola = prompt("Inserisci una parola:");
+console.log(parola)
+
+function palindroma (parola) {
+
+    let parolaReverse = parola.split("").reverse().join("");
+    console.log(`Parola invertita: ${parola.split("").reverse().join("")}`);
+    
+    return parola === parolaReverse;
+  }
+  
---
+++ palindroma/readme.md
@@ -0,0 +1,7 @@
+--chiedo all'utente di inserire una parola utilizzando un prompt
+
+--definisco una funzione che prende una parola come argomento e verifica se è palindroma
+
+--inverto la parola e verifico se è uguale confrontandola a quella iniziale
+
+--se è uguale stampo in console
